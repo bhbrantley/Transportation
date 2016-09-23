b1fa24aa3322cca9b6146f07c10d3f9788e6c257
Brittain Brantley <brittain.brantley@gmail.com>
2016-09-23 16:36:06 -0400

    next bus script to pull predicted minutes until next bus for specific stop

---
+++ next_bus.py
@@ -0,0 +1,34 @@
+# Simple script to talk to the WMATA API and
+# find out the number of elevators out of service
+# on the Green Line today.
+#
+from __future__ import print_function
+import json
+try:
+    from urllib2 import urlopen, Request
+except ImportError:
+    from urllib.request import urlopen, Request
+
+demo_api_key = '6b700f7ea9db408e9745c207da7ca827'
+
+url = 'https://api.wmata.com/NextBusService.svc/json/jPredictions?StopID=1001498'	
+
+hdrs = {'api_key': demo_api_key} 
+
+Stop_ID = '1001498'
+params = {'StopID': Stop_ID}
+
+req = Request(url, headers=hdrs)
+
+response = urlopen(req)
+raw_data = response.read()
+string_data = raw_data.decode('utf8')
+data = json.loads(string_data)
+
+predictions = data['Predictions']
+
+for p in predictions:
+	if p['DirectionText'] == "South to Archives":
+		time = p['Minutes']
+	print(time)
+
